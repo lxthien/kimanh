52be445e289e0e1a5ebda2d68298c45a75518eae
Thien Le <lxthien@gmail.com>
2025-11-06 16:17:56 +0700

    [06112026]

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -546,7 +546,7 @@ class NewsController extends Controller
     }
 
     private function lazyloadContent($post) {
-        $content = $post->getContents();
+        $content = htmlspecialchars($post->getContents());
         $dom = new \DOMDocument();
 
         // set error level
