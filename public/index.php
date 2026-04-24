9783026b3f5f5d08de9989c3aa3df3ec25004396
Thien Le <lxthien@gmail.com>
2026-04-24 11:11:11 +0700

    Symfony 3.3 to 4.4

--- public/index.php
+++ public/index.php
@@ -6,6 +6,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+use Symfony\Component\Dotenv\Dotenv;
+
+if (!isset($_SERVER['APP_ENV'])) {
+    if (!class_exists(Dotenv::class)) {
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+    }
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+}
+
 if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
     umask(0000);
 
